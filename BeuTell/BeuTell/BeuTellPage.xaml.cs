974cfe4cb489282ca4cf3e0092c00bac63edb814
O.Mrost <olgamrost@gmail.com>
2016-10-21 18:56:44 +0200

    Updated user interface

--- BeuTell/BeuTell/BeuTellPage.xaml.cs
+++ BeuTell/BeuTell/BeuTellPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
 
 namespace BeuTell
 {
@@ -7,6 +9,26 @@ namespace BeuTell
 		public BeuTellPage()
 		{
 			InitializeComponent();
+
+			var people = new List<Data> {
+				new Data ("Steve"),
+				new Data ("Steve1"),
+				new Data ("Steve2"),
+				new Data ("Steve3"),
+				new Data ("Steve4"),
+				new Data ("Steve5"),
+				new Data ("Steve6"),
+				new Data ("Steve7"),
+				new Data ("Steve8"),
+				new Data ("Steve9"),
+				new Data ("Stevtze"),
+				new Data ("Stevrte"),
+				new Data ("sdfs")
+
+			};
+
+			newListView.ItemsSource = people;
+
 		}
 	}
 }
---
+++ BeuTell/BeuTell/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace BeuTell
+{
+	public class Data
+	{
+
+		public string Name { get; private set; }
+
+		public Data(string name)
+		{
+			Name = name;
+
+		}
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
